993918e525812c9e5f9cb9f88480ea74f552d20e
Sam Revanoor <sam@trint.com>
2021-07-31 11:20:22 -0400

    makes change to test deployment

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.DATABASE_URL_HOSTED, {
+mongoose.connect(process.env.DATABASE_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
