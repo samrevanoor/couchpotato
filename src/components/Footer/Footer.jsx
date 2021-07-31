22700d324597f24b7eee178acfceb4d693503a89
Sam Revanoor <sam@trint.com>
2021-07-31 13:46:50 -0400

    adds tmdb notice (mandatory)

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./Footer.css";
+import image from "./tmdb.png"
 
 function Footer(props) {
   return (
     <div className="Footer-body">
-      <span className="Footer-credits">this app is powered by tmdb</span><br/>
-      <span className="Footer-name"> & made with ♥ by sam revanoor</span>
+      <span className="Footer-credits">this app is powered by <img src={image} alt="logo" width="100px"/></span><br/>
+      <span className="Footer-name"> & made with ♥ by sam revanoor</span><br />
+      <span className="Footer-notice">This product uses the TMDb API but is not endorsed or certified by TMDb.</span>
     </div>
   );
 }
